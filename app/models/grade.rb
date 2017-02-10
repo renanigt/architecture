bc3179ca9cf61133dc49ead23fa7b3544a54fb53
Renan Montenegro <renanigt@gmail.com>
2017-02-10 00:01:14 -0300

    Adding some basics validations

--- app/models/grade.rb
+++ app/models/grade.rb
@@ -1,4 +1,6 @@
 class Grade < ApplicationRecord
+  validates :grade, :project_id, presence: true
+
   def archive
     self.archived = true
     self.archived_date = DateTime.now
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,8 @@
 class Project < ApplicationRecord
+  validates :name, presence: true
+
   has_many :grades, :dependent => :destroy
+  
   accepts_nested_attributes_for :grades
 
   def done
